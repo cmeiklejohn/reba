e68ffa7e913e057e2c9d5197c8e3a2ab854e0473
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-10-15 01:19:51 -0400

    Pedantic cleanup.

--- src-cljs/reba/core.cljs
+++ src-cljs/reba/core.cljs
@@ -2,12 +2,12 @@
   (:require [clojure.string :as string]))
 
 (defn render-watcher! [x y a b]
-  "Watcher function, which applies the render function to
-  the iref it's bound to and stashes the rendered content
-  back in the objects meta data.  The render function, which is
-  applied is assumed to be referentially transparent."
+  "Watcher function, which applies the render function to the iref it's
+  bound to and stashes the rendered content back in the objects meta
+  data. The render function, which is applied is assumed to be
+  referentially transparent."
   (alter-meta! y (fn [m]
-    (merge m { :rendered (apply (:render-fn (meta y)) [b]) }))))
+    (merge m {:rendered (apply (:render-fn (meta y)) [b])}))))
 
 (defn main []
   (def my-name (atom (str "Chris")
