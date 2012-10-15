b62c7a0213706e5ce89b680e1ec376a07759c9cd
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-10-15 02:16:49 -0400

    Reorganize files, again.

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
                  [org.clojure/clojurescript "0.0-1424"]
                  [hiccups "0.1.1"]]
   :plugins [[lein-cljsbuild "0.2.7"]]
-  :source-path "src-clj"
+  :source-path "src/clj"
   :cljsbuild {
     :builds [{
-      :source-path "src-cljs"
+      :source-path "src/cljs"
       :compiler {
         :output-to "resources/public/application.js"
         :optimizations :whitespace
