b3e5827eafefab48d8dfaa9e88e1907b4ea5c437
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-10-15 02:02:50 -0400

    Extract concern for render-fn.
    render-fn should know about the string format, not necessarily the data
    structure, at least I think.

--- src-cljs/reba/core.cljs
+++ src-cljs/reba/core.cljs
@@ -4,27 +4,33 @@
   (:require-macros [hiccups.core :as hiccups]))
 
 (defn render-watcher! [x y a b]
-  "Watcher function, which applies the render function to the iref it's
+  "
+  Watcher function, which applies the render function to the iref it's
   bound to and stashes the rendered content back in the objects meta
   data. The render function, which is applied is assumed to be
-  referentially transparent."
+  referentially transparent.
+  "
   (alter-meta! y (fn [m]
     (merge m {:rendered (apply (:render-fn (meta y)) [b])}))))
 
 (defn render! [x initial render-fn]
-  "Given an iref, store a render function and bind a watcher to call
+  "
+  Given an iref, store a render function and bind a watcher to call
   that function on change.  Immediately execute the watcher with initial
-  value."
+  value.
+
+  render-fn will be called like so (apply render-fn changed-value)
+  "
   (alter-meta! x (fn [m] (merge m {:render-fn render-fn})))
   (add-watch x :render-watcher render-watcher!)
   (swap! x (fn [] initial)))
 
 (defn generate-names [names]
   "Generate the HTML elements for the names list."
-  (hiccups/html [:ul (for [x names] [:li x])]))
+  [:ul (for [x names] [:li x])])
 
 (defn main []
   (def names (atom []))
-  (render! names ["Chris" "Simon"] generate-names)
+  (render! names ["Chris" "Simon"] (fn [x] (hiccups/html (generate-names x))))
   (swap! names (fn [] ["Chris" "Simon" "Joan"]))
   (.log js/console (:rendered (meta names))))
