6a7ee675ed36eb830ba2a5e1ddcaaeee6aa822e1
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-10-15 01:03:14 -0400

    Update description.

--- src-cljs/reba/core.cljs
+++ src-cljs/reba/core.cljs
@@ -2,8 +2,12 @@
   (:require [clojure.string :as string]))
 
 (defn render-watcher! [x y a b]
-  "Watcher which executes the render function when changed."
-  (alter-meta! y (fn [m] (merge m { :rendered (apply (:render-fn (meta y)) [b]) }))))
+  "Watcher function, which applies the render function to
+  the iref it's bound to and stashes the rendered content
+  back in the objects meta data.  The render function, which is
+  applied is assumed to be referentially transparent."
+  (alter-meta! y (fn [m]
+    (merge m { :rendered (apply (:render-fn (meta y)) [b]) }))))
 
 (defn main []
   (def my-name (atom (str "Chris")
