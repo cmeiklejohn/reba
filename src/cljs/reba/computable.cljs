98a007d8a08034a1e937c0c728a6fbe83c11a5da
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-05 01:09:54 -0500

    Update computables with new API and observable semantics.

--- src/cljs/reba/computable.cljs
+++ src/cljs/reba/computable.cljs
@@ -8,12 +8,12 @@
 (defprotocol Computable
   "Computable protocol, which provides a mechanism to bind objects so
   that changes propagate to associated objects."
-  (bind! [source destination]
+  (add! [source destination derivation-name derivation-fn]
          "Create a binding between two objects, whereby changes to the
          first object propgate to the bound object."))
 
 (extend-type Atom
   Computable
-  (bind! [source destination derivation-name derivation-fn]
+  (add! [source destination derivation-name derivation-fn]
     (observable/add! source derivation-name
-                     (fn [] (swap! destination derivation-fn)))))
+                     (fn [x] (swap! destination (fn [] (apply derivation-fn [x])))))))
