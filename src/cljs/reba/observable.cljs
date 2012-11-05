d84329e85533659625a23efe3599330614988434
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-05 01:09:02 -0500

    Redfine observers so they only work with the new value of objects.

--- src/cljs/reba/observable.cljs
+++ src/cljs/reba/observable.cljs
@@ -15,6 +15,6 @@
 (extend-type Atom
   Observable
   (add! [object observer-name observer-fn]
-    (add-watch object observer-name observer-fn))
+    (add-watch object observer-name (fn [x y a b] (apply observer-fn [b]))))
   (remove! [object observer-name]
     (remove-watch object observer-name)))
