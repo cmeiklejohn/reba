e21ca89c4a01504273efe85fb62e81eb7bbc0a47
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-06 11:45:12 -0500

    Fix up the project file.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject reba "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject reba "0.0.1-SNAPSHOT"
+  :description "Prototype of a binding library authored in ClojureScript."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojurescript "0.0-1424"]
                  [hiccups "0.1.1"]]
   :plugins [[lein-cljsbuild "0.2.7"]]
   :source-path "src/clj"
+  :hooks [leiningen.cljsbuild]
   :cljsbuild {
     :builds [{
       :source-path "src/cljs"
